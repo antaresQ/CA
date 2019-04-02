43bae4e672ecd0ced1fc5b71de89c82452e7825e
Qhairil Mundzir <antares_elementium@hotmail.com>
2019-04-02 12:50:56 +0800

    Added attribute based route

--- 13AShopCart/13AShopCart/App_Start/RouteConfig.cs
+++ 13AShopCart/13AShopCart/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace _13AShopCart
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
