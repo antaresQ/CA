53ea22427f5265e56bcbf4885c2ed807646c8002
Qhairil Mundzir <antares_elementium@hotmail.com>
2019-04-02 15:30:25 +0800

    Gallery COntroller and View

--- 13AShopCart/13AShopCart/Controllers/GalleryController.cs
+++ 13AShopCart/13AShopCart/Controllers/GalleryController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _13AShopCart.Models;
+using _13AShopCart.DB;
 
 namespace _13AShopCart.Controllers
 {
     public class GalleryController : Controller
     {
         // GET: Gallery
+
         public ActionResult Index()
         {
+            List<Product> products = ProductData.GetProducts();
+
+            string sessionId = Guid.NewGuid().ToString();
+            string cartId = Guid.NewGuid().ToString();
+
+            ViewData["sessionId"] = sessionId;
+            ViewData["cartId"] = cartId;
+            ViewData["products"] = products;
+
             return View();
         }
     }
