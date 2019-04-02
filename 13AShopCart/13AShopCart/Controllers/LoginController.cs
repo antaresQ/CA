b7f825bb4bf2af0df00ac04e548a15726160340c
alfredykh <alfredykh@users.noreply.github.com>
2019-04-02 11:17:32 +0800

    Creating DB
    I'm creating a DB folder, consist of:
    Data.cs,
    UserData.cs,
    SessionData.cs,
    ProductData.cs,
    PurchaseData.cs,
    Code.cs

--- 13AShopCart/13AShopCart/Controllers/LoginController.cs
+++ 13AShopCart/13AShopCart/Controllers/LoginController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using _13AShopCart.DB;
+using _13AShopCart.Models;
+using System.Diagnostics;
 
 namespace _13AShopCart.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Index()
+        public ActionResult Index(string Username, string Password)
         {
-            return View();
+            if (Username == null)
+                return View();
+
+            User user = UserData.GetUserByUserUsername(Username);
+            if (user.Password != Password)
+                return View();
         }
     }
 }
