f87af4342a2ab3605f13a18c7a7e8230333942d3
Qhairil Mundzir <antares_elementium@hotmail.com>
2019-04-04 09:05:52 +0800

    Update to Sync all files

--- 13AShopCart/13AShopCart/DB/Data.cs
+++ 13AShopCart/13AShopCart/DB/Data.cs
@@ -8,7 +8,7 @@ namespace _13AShopCart.DB
     public class Data
     {
         // Change the SERVER Name to what is shown on your SQL Server Management Studio
-        public static string connectionString = "Server=DESKTOP-IDOQ8TL;" +
+        public static string connectionString = "Server=ANTQ-NBPRO;" +
                 "Database=mvcShop; Integrated Security=true";
     }
 }
