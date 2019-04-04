1333708c3b0398f06dab82609324ec6344848c97
Qhairil Mundzir <antares_elementium@hotmail.com>
2019-04-04 13:08:19 +0800

    Nan's cart

--- 13AShopCart/13AShopCart/Controllers/CartController.cs
+++ 13AShopCart/13AShopCart/Controllers/CartController.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace _13AShopCart.Controllers
+using Cart.Models;
+using Cart.DB;
+
+namespace Cart.Controllers
 {
     public class CartController : Controller
     {
         // GET: Cart
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*public ActionResult ViewCart(List<CartItem> c)
+        {
+            List<CartItem> items = CartItem.GetCart();
+
+            ViewData["Items"] = items;
+            return View();
+        }*/
+        /*public ActionResult ViewCart(List<CartItem> cartItems)
+        {
+            List<CartItem> items = CartItem.SetCart(cartItems);
+
+            ViewData["Items"] = items;
+            return View();
+        }*/
+
+        public ActionResult ViewCart(int cartID)
+        {
+            List<CartItem> items = CartData.GetCartDetailsByCartId(cartID);
+
+            ViewData["Items"] = items;
+            return View();
+        }
+        public ActionResult Checkout(int cartID)
+        {
+            List<CartItem> items = CartData.GetCartDetailsByCartId(cartID);
+            CartData.WriteItemsToPurchase(items);
+            return View();
+        }
+        public ActionResult Gallery()
         {
             return View();
         }
